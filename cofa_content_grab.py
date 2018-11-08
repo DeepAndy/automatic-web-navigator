414540044f7015f0cd3a19bb0940db4c1f0d79e8
Austin Moore <am058613@ohio.edu>
2018-11-08 17:04:13 -0500

    Allowed for pasting back to body textarea

--- cofa_content_grab.py
+++ cofa_content_grab.py
@@ -8,42 +8,46 @@ import getpass
 import time
 
 def script_main(driver):
-        source = driver.page_source.encode("ascii", "ignore")
+	source = driver.page_source.encode("ascii", "ignore")
 
-        source = source.replace("&nbsp;", "")
+	source = source.replace("&nbsp;", "")
 
-        soup = BeautifulSoup(source, features="html.parser")
+   	soup = BeautifulSoup(source, features="html.parser")
 
-        content = soup.find("div", id="cofa_contentCol1")
+   	content = soup.find("div", id="cofa_contentCol1")
 
-        for script in content.find_all("script"):
-                script.decompose()
+   	for script in content.find_all("script"):
+   		script.decompose()
 
-        for style in content.find_all("style"):
-                style.decompose()
+   	for style in content.find_all("style"):
+   		style.decompose()
 
-        for link in content.find_all("link"):
-                link.decompose()
+  	for link in content.find_all("link"):
+   	    link.decompose()
 
-        content = HTMLParser().unescape(content)
-        content = StringIO.StringIO(content)
-        content = content.readlines()
+ 	content = HTMLParser().unescape(content)
+ 	content = StringIO.StringIO(content)
+  	content = content.readlines()
 
-        lines, errors, warnings, print_friendly_errors, error_line_string = find_errors(content)
-        fix_all(lines, errors)
+ 	lines, errors, warnings, print_friendly_errors, error_line_string = find_errors(content)
+  	fix_all(lines, errors)
         
-        output = ""
+   	output = ""
 
-        for line in lines:
-                output += line + "\n"
+   	for line in lines:
+		line = line.strip()
+		output += line
+
+	output = re.sub(r"'", "\\'", output)
 
 	cofa_main_page = "https://webcms.ohio.edu/fine-arts/admin/content"
 	cas_username_xpath = "//*[@id='username']"
 	cas_password_xpath = "//*[@id='password']"
 	cas_login_button_xpath = "/html/body/div[1]/div[2]/div/form/section[3]/div/button[1]"
-	add_content_xpath = "//*[@id='block-seven-local-actions']/ul/li/a"
-	basic_page_xpath = "//*[@id='block-seven-content']/ul/li[7]/a"
-	body_textarea_script = 'document.getElementsByTagName("body")[0].innerHTML = "' + output + '"'
+	basic_page_url = "https://webcms.ohio.edu/fine-arts/node/add/page"
+	body_textarea_xpath = "//textarea[@data-editor-value-original]"
+	body_textarea_script = "window.frames[0].document.getElementsByTagName('body')[0].innerHTML='" + output + "';"
+	save_xpath = "//*[@id='edit-submit']"
 
 	driver.get(cofa_main_page)
 
@@ -58,8 +62,6 @@ def script_main(driver):
 		element = driver.find_element_by_xpath(cas_login_button_xpath)
 		element.click()
 
-	element = driver.find_element_by_xpath(add_content_xpath)
-	element.click()
-	element = driver.find_element_by_xpath(basic_page_xpath)
-	element.click()
+	driver.get(basic_page_url)
+	time.sleep(1) # NEED TO WAIT FOR TEXTAREA TO LOAD
 	driver.execute_script(body_textarea_script)
