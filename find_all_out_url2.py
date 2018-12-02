8094c0f610ab94299587b0a1fda346d7cd51c530
Austin Moore <am058613@ohio.edu>
2018-12-01 21:50:09 -0500

    Finished the onedrive and box link parsing

--- find_all_out_url2.py
+++ find_all_out_url2.py
@@ -2,50 +2,60 @@ import pyexcel
 import re
 from bs4 import BeautifulSoup
 from selenium import webdriver
+import time
 
 def script_main(driver, url, pos):
     source = driver.page_source
     page_url = driver.current_url
     title = driver.title
     soup = BeautifulSoup(source, "html.parser")
-    decomissioned = False
+    decommissioned = False
+    page_not_found = False
     data = []
 
     for tag in soup.find_all("h1"):
         if (tag.text == "This page has been decommissioned"):
-            decomissioned = True
+            decommissioned = True
+        if (tag.text == "Aw, Nuts!"):
+            page_not_found = True
 
-    if (decomissioned == False):
+    if (decommissioned == False and page_not_found == False):
         first_href = True
         box_links = []
         onedrive_links = []
 
         for tag in soup.find_all("a"):
             if (tag.has_attr("href")):
-                if (re.findall("ohio.box.com", tag["href"]) and not re.findall("javascript", tag["href"])):
+                if (re.findall("ohio.box.com", tag["href"]) and tag["href"].find("javascript") != 0):
                     box_links.append(tag["href"])
                 if (re.findall("catmailohio.sharepoint.com", tag["href"])):
                     onedrive_links.append(tag["href"])
 
-        if (first_href == True):
-            if (len(box_links) >= 1 and len(onedrive_links) >= 1):
-                data.append([title, page_url, box_links[0], onedrive_links[0]])
-            elif (len(box_links) >= 1 and len(onedrive_links) == 0):
-                data.append([title, page_url, box_links[0], ''])
-            elif (len(box_links) == 0 and len(onedrive_links) >= 1):
-                data.append([title, page_url, '', onedrive_links[0]])
-            first_href = False
+        if (len(box_links) >= len(onedrive_links)):
+            end_of_loop = box_links
+            larger = "box"
         else:
-            if (len(box_links) >= len(onedrive_links)):
-                for index in range(1, len(box_links)):
+            end_of_loop = onedrive_links
+            larger = "onedrive"
+
+        for index in range(len(end_of_loop)):
+            if (first_href == True):
+                if (len(box_links) >= 1 and len(onedrive_links) >= 1):
+                    data.append([title, page_url, box_links[0], onedrive_links[0]])
+                elif (len(box_links) >= 1 and len(onedrive_links) == 0):
+                    data.append([title, page_url, box_links[0], ''])
+                elif (len(box_links) == 0 and len(onedrive_links) >= 1):
+                    data.append([title, page_url, '', onedrive_links[0]])
+                first_href = False
+            else:
+                if (larger == "box"):
                     if (index < len(box_links) and index < len(onedrive_links)):
                         data.append(['', '', box_links[index], onedrive_links[index]])
                     elif (index < len(box_links) and index >= len(onedrive_links)):
                         data.append(['', '', box_links[index], ''])
                     elif (index >= len(box_links) and index < len(onedrive_links)):
                         data.append(['', '', '', onedrive_links[index]])
-            else:
-                for index in range(1, len(onedrive_links)):
+                elif (larger == "onedrive"):
                     if (index < len(box_links) and index < len(onedrive_links)):
                         data.append(['', '', box_links[index], onedrive_links[index]])
                     elif (index < len(box_links) and index >= len(onedrive_links)):
@@ -53,6 +63,10 @@ def script_main(driver, url, pos):
                     elif (index >= len(box_links) and index < len(onedrive_links)):
                         data.append(['', '', '', onedrive_links[index]])
 
-        print(data)
-    else:
+        sheet = pyexcel.get_sheet(file_name="example.xlsx")
+        sheet.row += data
+        sheet.save_as("example.xlsx")
+    elif (decommissioned == True):
         print("DECOMISSIONED")
+    elif (page_not_found == True):
+        print("AW, NUTS!")
