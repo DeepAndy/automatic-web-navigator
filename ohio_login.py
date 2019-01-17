e596145d8938e49d8178a32a9a4e7cb15948aaa0
Austin Moore <am058613@ohio.edu>
2019-01-17 15:05:46 -0500

    Added xpath for username and password

--- ohio_login.py
+++ ohio_login.py
@@ -15,6 +15,10 @@ Description:    Asks user for OHIO login ID and password. Completes the login
                 by Selenium
 '''
 def ohio_login():
+    cas_username_xpath = "//*[@id='username']"
+    cas_password_xpath = "//*[@id='password']"
+    cas_login_button_xpath = "/html/body/div[1]/div[2]/div/form/section[3]/div/button[1]"
+
     if (re.findall(r"cas.sso.ohio.edu", str(driver.current_url))):
         username = raw_input("Enter OHIO username: ")
         password = getpass.getpass("Enter OHIO password: ")
