7a80d7a020f542e8a54d29b0842318d14e57e5bc
Austin Moore <am058613@ohio.edu>
2019-01-17 16:15:48 -0500

    Added comment header and script_main

---
+++ fix_html_mass.py
@@ -0,0 +1,9 @@
+'''
+Author:         Austin Moore
+Script Type:    Companion Script
+Description:    This will take Drupal nodes as input and clean the HTML given
+                in any rich text editor
+'''
+
+def script_main(driver, url, pos):
+    
