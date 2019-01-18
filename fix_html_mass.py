e56d0252d21f859c0550bcd111dcce08bec90364
Austin Moore <am058613@ohio.edu>
2019-01-18 10:18:08 -0500

    Adjusted login and tweaking fix_html_mass

--- fix_html_mass.py
+++ fix_html_mass.py
@@ -2,8 +2,21 @@
 Author:         Austin Moore
 Script Type:    Companion Script
 Description:    This will take Drupal nodes as input and clean the HTML given
-                in any rich text editor
+                in any rich text editor. Used in conjunction with navigator.py
 '''
 
+import re
+from bs4 import BeautifulSoup
+from ohio_login import ohio_login
+
 def script_main(driver, url, pos):
-    
+    ohio_login(driver)
+    driver.get(url)
+
+    page_source = driver.page_source
+    page_source = page_source.replace(u"\xa0", u"")
+    page_source = page_source.replace(u"\xc2", u"")
+    soup = BeautifulSoup(page_source, features="html.parser")
+
+    for tag in soup.find_all("iframe", {"class": "cke_wysiwyg_frame cke_reset"}):
+        print(tag)
--- html_cleanup_mass.py
+++
@@ -1,9 +0,0 @@
-'''
-Author:         Austin Moore
-Script Type:    Companion Script
-Description:    This script is intended to correct HTML in mass given a Drupal
-                node as input to navigator.py
-'''
-
-def script_main(driver, url, pos):
-    
