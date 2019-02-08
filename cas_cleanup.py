fe568e04281eb18d8d351ac4e3d7419f3a1c9594
Austin Moore <am058613@ohio.edu>
2019-02-08 11:14:23 -0500

    Added group checking for parent pages

--- cas_cleanup.py
+++ cas_cleanup.py
@@ -35,6 +35,77 @@ def script_main(driver, received_url, pos):
     rich_text_body_xpath = '/html/body'
     first = True
 
+    if (received_url.find("/cas/group/6/nodes")):
+        parent_page = "/cas/sociology-anthropology"
+    elif (received_url.find("/cas/group/11/nodes")):
+        parent_page = "/cas/political-science"
+    elif (received_url.find("/cas/group/16/nodes")):
+        parent_page = "/cas/geology"
+    elif (received_url.find("/cas/group/21/nodes")):
+        parent_page = "/cas/history"
+    elif (received_url.find("/cas/group/26/nodes")):
+        parent_page = "/cas/nqpi"
+    elif (received_url.find("/cas/group/31/nodes")):
+        parent_page = "/cas/linguistics"
+    elif (received_url.find("/cas/group/36/nodes")):
+        parent_page = "/cas/about"
+    elif (received_url.find("/cas/group/41/nodes")):
+        parent_page = "/cas/dean"
+    elif (received_url.find("/cas/group/46/nodes")):
+        parent_page = "/cas/contemporary-history"
+    elif (received_url.find("/cas/group/51/nodes")):
+        parent_page = "/cas/law-center"
+    elif (received_url.find("/cas/group/56/nodes")):
+        parent_page = "/cas/plant-biology"
+    elif (received_url.find("/cas/group/61/nodes")):
+        parent_page = "/cas/psychology"
+    elif (received_url.find("/cas/group/66/nodes")):
+        parent_page = "/cas/english"
+    elif (received_url.find("/cas/group/71/nodes")):
+        parent_page = "/cas/chemistry"
+    elif (received_url.find("/cas/group/76/nodes")):
+        parent_page = "/cas/wgss"
+    elif (received_url.find("/cas/group/81/nodes")):
+        parent_page = "/cas/graduate"
+    elif (received_url.find("/cas/group/86/nodes")):
+        parent_page = "/cas/biology"
+    elif (received_url.find("/cas/group/91/nodes")):
+        parent_page = "/cas/physics-astronomy"
+    elif (received_url.find("/cas/group/96/nodes")):
+        parent_page = "/cas/african-american-studies"
+    elif (received_url.find("/cas/group/101/nodes")):
+        parent_page = "/cas/undergraduate"
+    elif (received_url.find("/cas/group/106/nodes")):
+        parent_page = "/cas/philosophy"
+    elif (received_url.find("/cas/group/111/nodes")):
+        parent_page = "/cas/classics"
+    elif (received_url.find("/cas/group/116/nodes")):
+        parent_page = "/cas/economics"
+    elif (received_url.find("/cas/group/121/nodes")):
+        parent_page = "/cas/math"
+    elif (received_url.find("/cas/group/126/nodes")):
+        parent_page = "/cas/genomics"
+    elif (received_url.find("/cas/group/131/nodes")):
+        parent_page = "/cas/alumni"
+    elif (received_url.find("/cas/group/136/nodes")):
+        parent_page = "/cas/contact"
+    elif (received_url.find("/cas/group/141/nodes")):
+        parent_page = "/cas/phi-beta-kappa"
+    elif (received_url.find("/cas/group/146/nodes")):
+        parent_page = "/cas/molecular-cellular-biology"
+    elif (received_url.find("/cas/group/151/nodes")):
+        parent_page = "/cas/ocees"
+    elif (received_url.find("/cas/group/156/nodes")):
+        parent_page = "/cas/quantitative-biology"
+    elif (received_url.find("/cas/group/161/nodes")):
+        parent_page = "/cas/ping-institute"
+    elif (received_url.find("/cas/group/166/nodes")):
+        parent_page = "/cas/modern-languages"
+    elif (received_url.find("/cas/group/171/nodes")):
+        parent_page = "/cas/opie"
+    elif (received_url.find("/cas/group/176/nodes")):
+        parent_page = "/cas/geography"
+
     # Get title for the slug
     wait.until(EC.presence_of_element_located((By.XPATH, title_xpath)))
     title = driver.find_element_by_xpath(title_xpath).get_attribute("value")
@@ -68,7 +139,6 @@ def script_main(driver, received_url, pos):
     content = content.replace('"', '\\"')
     content = content.strip()
     content = content.replace("\n", "")
-    print(content)
 
     body_textarea_script = 'document.getElementsByTagName("body")[0].innerHTML="' + str(content) + '";'
     driver.execute_script(body_textarea_script)
