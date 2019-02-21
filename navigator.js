bcaa8f5e63db516fe1cae7d60a92ab7e74db2039
Austin Moore <am058613@ohio.edu>
2019-02-21 16:40:31 -0500

    Added menus and printing waq directory

--- navigator.js
+++ navigator.js
@@ -5,13 +5,110 @@
  *                 performed on them
  * Notes:          In development. This script is intended to run website-action
                    queues with Node.js companion scripts.
+ * Packages:       readline-sync
  */
 
-function menu()
+ class queue
+ {
+    constructor(web_queue, action_queue, web_action_queue)
+    {
+        this.web_queue = web_queue;
+        this.action_queue = action_queue;
+        this.web_action_queue = web_action_queue;
+    }
+ }
+
+function is_numeric(value)
 {
-    console.log("1.  Load a queue");
-    console.log("2.  Print a queue");
-    console.log("3.  Run website-action queue");
+    return !isNaN(value);
 }
 
-menu()
+function load_queue(queues, queue_type)
+{
+    switch(queue_type)
+    {
+        case "web_action_queue": 
+            let fs = require("fs");
+            fs.readdirSync("./web-action-queues/").forEach(file => {
+                console.log(file);
+            });
+            break;
+        default: process.exit(1); // This would be my fault
+    }
+}
+
+function load_queue_menu(queues)
+{
+    let input = "";
+
+    while (input === "")
+    {
+        console.log("\n----------------------------------------------------")
+        console.log("1.  Load website-action queue");
+        console.log("2.  Back")
+        console.log("----------------------------------------------------")
+
+        input = readline_sync.question("\nSelect an option: ");
+
+        if (is_numeric(input) === true)
+        {
+            input = parseInt(input);
+
+            switch(input)
+            {
+                case 1: load_queue(queues, "web_action_queue");
+                case 2: return;
+                default: console.log("Invalid number.");
+                         input = "";
+                         continue;
+            }
+        }
+        else
+        {
+            input = "";
+            continue;
+        }
+    }
+}
+
+function menu(queues)
+{
+    let input = "";
+
+    while (input === "")
+    {
+        console.log("\n----------------------------------------------------")
+        console.log("1.  Load a queue");
+        console.log("2.  Print a queue");
+        console.log("3.  Run website-action queue");
+        console.log("4.  Quit")
+        console.log("----------------------------------------------------")
+
+        input = readline_sync.question("\nSelect an option: ");
+
+        if (is_numeric(input) === true)
+        {
+            input = parseInt(input);
+
+            switch(input)
+            {
+                case 1: load_queue_menu(queues);
+                        input = "";
+                        continue;
+                case 4: process.exit(0);
+                default: console.log("Invalid number."); 
+                         input = "";
+                         continue;
+            }
+        }
+        else
+        {
+            input = "";
+            continue;
+        }
+    }
+}
+
+const readline_sync = require("readline-sync");
+let queues = new queue([], [], [[]]);
+menu(queues)
