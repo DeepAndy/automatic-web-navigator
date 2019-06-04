00ae149df426659bb32a7251555b7dca1ab7dca8
Austin Moore <am058613@ohio.edu>
2019-06-04 14:07:05 -0400

    Removed downloaders but keeping download_image_document

--- download_documents.py
+++
@@ -1,5 +0,0 @@
-from download_image_document import download_documents_from_url
-
-url = input('Enter a URL: ')
-download_documents_from_url(url)
-input('\nPress <ENTER> to exit')
--- download_images.py
+++
@@ -1,5 +0,0 @@
-from download_image_document import download_images_from_url
-
-url = input('Enter a URL: ')
-download_images_from_url(url)
-input('\nPress <ENTER> to exit')
