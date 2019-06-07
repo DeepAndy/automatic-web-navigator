e4e7c7bb3069b4977370577a3ae28599991a9b4a
Austin Moore <am058613@ohio.edu>
2019-06-07 10:44:46 -0400

    Moved OU specific files to new repository

--- legacy-javascript/lrc-rename.js
+++
@@ -1,2 +0,0 @@
-document.querySelectorAll("input[value='/linguistics/computer-labs/']")[0].click();
-document.getElementById('edit-submit').click()
--- legacy-main/education_profiles_rename.py
+++
@@ -1,7 +0,0 @@
-f = open("web-queues/education_profiles.wq", "r")
-f1 = open("web-queues/education_profiles_1.wq", "w")
-lines = f.readlines()
-url = "https://www.ohio.edu/education/faculty-and-staff/"
-
-for line in lines:
-    f1.write(url + line)
